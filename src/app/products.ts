f7c61ba2e4503c309213cc654827b0349d63352c
askakps <92042567+askakps@users.noreply.github.com>
2023-02-18 12:35:10 +0600

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,31 +1,26 @@
 export interface Product {
   id: number;
   name: string;
-  price: number;
   description: string;
+  image: string;
+  link: string;
 }
 
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Ноутбук Apple MacBook Air 13 MGN63 серый',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h65/h0f/33125684084766/apple-macbook-air-2020-13-3-mgn63-seryj-100797845-1-Container.jpg',
+    description: `Диагональ экрана: 13.3 дюйм<
+    Процессор: Apple M1
+    Видеокарта: Apple M1 7 core
+    Размер оперативной памяти: 8 ГБ
+    Тип жесткого диска: SSD
+    Общий объем накопителей: 256 ГБ`,
+    link: 'https://kaspi.kz/shop/p/apple-macbook-air-13-mgn63-seryi-100797845/?c=750000000#!/item',
   },
-  {
-    id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
-  }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
